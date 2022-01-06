2dae6c3631027fa702979c902c0e07758f42926b
Adarsh Tyagi <adarshtyagi044@gmail.com>
2022-01-06 21:52:56 +0530

    started: header component

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./Header.css"
-import CancelIcon from '@mui/icons-material/Cancel';
+import CloseIcon from '@mui/icons-material/Close';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
+    const [search, setSearch] = useState("")
+    const [show, setShow] = useState(false)
+
+    const submitHandler = () => {
+
+    }
+
+    const toggleSearchIcon = () => {
+        window.innerWidth <= 800 ? setShow(true) : setShow(false)
+    }
+
+    const closeHandler = () => {
+        setSearch("")
+        if (window.innerWidth <= 800) {
+            setShow(true)
+        }
+    }
+
+    useEffect(() => {
+        toggleSearchIcon()
+        window.addEventListener("resize", toggleSearchIcon)
+        return () => {
+            window.removeEventListener("resize", toggleSearchIcon)
+        }
+    }, [])
+
     return (
         <div className='header'>
-            <div className='header__left'>
-                <a href="/">NotesApp</a>
-            </div>
-            <div className='header__middle'>
-                <div>
-                    <input type="text" placeholder='Search' />
-                    <CancelIcon />
+
+            {window.innerWidth <= 800 && !show ? "" : (
+                <div className='header__left'>
+                    <a href="/">NotesApp</a>
                 </div>
+            )}
+
+            <div className='header__middle'>
+                {show ? <SearchIcon onClick={() => setShow(false)} /> :
+                <form onSubmit={submitHandler}>
+                    <input type="text" placeholder='Search' onChange={e => setSearch(e.target.value)} value={search} />
+                    <CloseIcon onClick={closeHandler} />
+                </form>}
             </div>
+            
             <div className='header__right'>
                 <img src='/image/defaultProfile.svg' alt="profile" />
             </div>
+
         </div>
     )
 }
