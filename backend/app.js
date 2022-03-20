c9de4d3ec2718cf7890b22148be51a1e989425de
Adarsh Tyagi <adarshtyagi044@gmail.com>
2022-03-20 16:58:05 +0530

    fix: image upload feature

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,9 @@ const noteRouter = require("./routes/noteRoutes");
 
 // initializing app
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(fileUpload());
 app.use(cors());
 
